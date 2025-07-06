f1ebfb72e125341452e2b668692f9aca24c79008
Chandru <cyberchandru87@gmail.com>
2025-07-07 00:26:47 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/chandrumahi/',
   server: {
     host: "::",
     port: 8080,
@@ -19,4 +20,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  
 }));
